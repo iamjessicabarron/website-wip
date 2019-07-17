4be4b7ef1969bc0c03fceb98e2d3bfb7e8c7b744
Jessica Barron <iamjessicabarron@gmail.com>
2019-07-17 10:20:56 +1000

    Redesigned look of timeline items

--- src/Timeline/TimelineElement.tsx
+++ src/Timeline/TimelineElement.tsx
@@ -23,16 +23,24 @@ const TimelineElement: React.FC<Props> = (props) => {
   })
 
     return (
-      <div className="timelineElementsContainer">
-        <div className="timelineElement">
-          <h1>{`${props.year} /`}</h1>
-          <h2>{props.title}</h2>
-          <h3>{props.desc}</h3>
-          {linkElement != null && linkElement}
-        </div>
-        <div className="timelineElementSecondary">
+      <div className="timelineItem">
+        {/* <div className="timelineElement"> */}
+          <h2>{`${props.year}`}</h2>
+          <h1>{props.title}</h1>
+          <div className="techContainer">
+            <div className="spanContainer">
+              <div>
+                {techElements}
+              </div>
+            </div>
+          </div>
+          <div className="contentContainer">
+            <h3>{props.desc}</h3>
+            {linkElement != null && linkElement}
+          </div>
+        {/* </div> */}
+        <div className="itemImage">
           <img src={props.image} />
-          {techElements}
         </div>
       </div>
     );
